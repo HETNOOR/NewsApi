8a369bec3e2b3009799bc3c059378f32f7f1df80
HETNOOR <daffixipop@gmail.com>
2024-11-13 17:43:33 +0700

    Initial Commit

---
+++ NewsApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApi
+//
+//  Created by Максим Герасимов on 13.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
